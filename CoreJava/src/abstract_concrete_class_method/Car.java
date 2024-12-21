49ea057804355da1f34828da40042d75d6dfb692
Arun Kumar A S <arun1998.ag12@gmail.com>
2024-12-22 00:04:44 +0530

    Test

--- CoreJava/src/abstract_concrete_class_method/Car.java
+++ CoreJava/src/abstract_concrete_class_method/Car.java
@@ -5,5 +5,9 @@ public abstract class Car extends Vehicle {
 	abstract void stop();
 	
 	// start() and shiftGear() implemented implicitly
+	
+	public void toDisplay() {
+		System.out.println("Hello Java");
+	}
 
 }
